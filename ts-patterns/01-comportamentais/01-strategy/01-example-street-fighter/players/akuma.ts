4e344949478ff0624bc2cf07896bc86ac1d618a0
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 17:33:21 -0300

    refactor: remove attack method implementation from child classes

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
@@ -3,8 +3,4 @@ import { Fighter } from "../../abstracts/fighter";
 export class Akuma extends Fighter {
     name = 'Akuma';
     attackName = 'Gou-Hadoken'
-
-    attack(): string {
-        return `${this.name} attacks with ${this.attackName}!`
-    }
 }
--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/balrog.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/balrog.ts
@@ -3,8 +3,4 @@ import { Fighter } from "../../abstracts/fighter";
 export class Balrog extends Fighter {
     name = 'Balrog';
     attackName = 'Dash Straight'
-   
-    attack(): string {
-        return `${this.name} attacks with ${this.attackName}!`
-    }
 }
