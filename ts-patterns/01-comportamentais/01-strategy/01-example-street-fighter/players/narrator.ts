b35cf60f131eb97aef8dbcc5eb029201f48189c9
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 20:08:57 -0300

    feat: narrator can tell a story

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/narrator.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/narrator.ts
@@ -1,8 +1,13 @@
 
 import { Player } from "../abstracts/player";
+import { IPlayer } from "../interfaces/Iplayer";
 
 export class Narrator extends Player {
     constructor() {
         super('Narrator')
+    }
+
+    tellStory(player: IPlayer) {
+        player.tellStory()
     }
 }
