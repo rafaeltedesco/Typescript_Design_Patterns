05ca86db27ff3c413c60a4a86ef6a204512e8ddd
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 17:11:04 -0300

    feat: add property attackName with Gou-Hadoken as value

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
@@ -2,6 +2,7 @@ import { Player } from "../interfaces/player";
 
 export class Akuma implements Player {
     name = 'Akuma';
+    attackName = 'Gou-Hadoken'
 
     attack(): string {
         return `${this.name} attacks with Gou-Hadoken!`
