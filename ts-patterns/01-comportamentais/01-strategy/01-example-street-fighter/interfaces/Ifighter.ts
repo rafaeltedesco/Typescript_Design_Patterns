573692aa4d0d67e4888816c3c41ce75490295c5e
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 18:52:20 -0300

    feat: add lifePoints and strength to IFighter interface

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/interfaces/Ifighter.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/interfaces/Ifighter.ts
@@ -1,6 +1,8 @@
 import { IPlayer } from "./Iplayer";
 
 export interface IFighter extends IPlayer {
+    lifePoints: number;
+    strength: number;
     attackName: string;
     attack(enemy: IFighter): string;
 }
