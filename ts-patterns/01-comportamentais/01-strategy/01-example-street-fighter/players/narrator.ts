52db73ca0290cb53f6bf1664a91a2160020fb9bf
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 20:23:24 -0300

    feat: narrator implements INarrator

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/narrator.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/narrator.ts
@@ -1,8 +1,10 @@
 
 import { Player } from "../abstracts/player";
+import { INarrator } from "../interfaces/InarratorPlayer";
 import { IPlayer } from "../interfaces/Iplayer";
 
-export class Narrator extends Player {
+export class Narrator extends Player implements INarrator {
+
     constructor() {
         super('Narrator')
     }
