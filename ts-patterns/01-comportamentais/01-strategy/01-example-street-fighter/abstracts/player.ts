1b867b5e37cf5ea8eb09e747a683143fa5c68de2
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 20:52:24 -0300

    refactor: class Player knows how to load your own story

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/abstracts/player.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/abstracts/player.ts
@@ -1,14 +1,34 @@
+import { readJSONFile } from "../../../utils/jsonFileReader";
 import { IPlayer } from "../interfaces/Iplayer";
 
 export abstract class Player implements IPlayer {
     isAlive = true
+    private _story = ''
+    private _storyPath = ''
     constructor(private _name: string) {}
 
     get name(): string {
         return this._name
     }
-
+    
     get story(): string {
-        return ''
+        return this._story;
     }
+    
+    set story(story: string) {
+        this._story = story
+    }
+
+    get storyPath(): string {
+        return this._storyPath
+    }
+
+    set storyPath(storyPath: string) {
+        this._storyPath = storyPath
+    }
+
+    async loadStory(): Promise<void> {
+        this.story = (await readJSONFile(this.storyPath)).story
+    }
+   
 }
