ecd607aab230053149b7f615aed9a6a686e04959
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 16:56:20 -0300

    test: refactor player instanciation

--- ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/unit/akuma.test.ts
+++ ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/unit/akuma.test.ts
@@ -2,12 +2,13 @@ import { Player } from "../../../../../01-comportamentais/01-strategy/01-example
 import { Akuma } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/players/akuma"
 
 describe('Test Akuma', ()=> {
+    
+    const player: Player = new Akuma()
+
     it('should create a player called Akuma', ()=> {
-        const player: Player = new Akuma()
         expect(player.name).toBe('Akuma')
     })
     it('should attack with Gou-Hadoken', ()=> {
-        const player: Player = new Akuma()
         expect(player.attack()).toEqual('Akuma attacks with Gou-Hadoken!')
     })
 })
