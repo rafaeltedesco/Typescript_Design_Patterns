e18a4112d1d140a0fe283cde9f49e763190f1b9c
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 16:58:29 -0300

    feat: implements attack method on Akuma

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
@@ -2,4 +2,8 @@ import { Player } from "../interfaces/player";
 
 export class Akuma implements Player {
     name = 'Akuma';
+
+    attack(): string {
+        return `${this.name} attacks with Gou-Hadoken!`
+    }
 }
