2d91a3808d1f39ad338aaf5e7a9e7cea822f40a4
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 19:28:08 -0300

    feat: Player implements IPlayer and add property is alive with default value as true

--- ts-patterns/01-comportamentais/01-strategy/abstracts/player.ts
+++ ts-patterns/01-comportamentais/01-strategy/abstracts/player.ts
@@ -1,5 +1,7 @@
+import { IPlayer } from "../01-example-street-fighter/interfaces/Iplayer";
 
-export abstract class Player {
+export abstract class Player implements IPlayer {
+    isAlive = true
     constructor(private _name: string) {}
 
     get name(): string {
