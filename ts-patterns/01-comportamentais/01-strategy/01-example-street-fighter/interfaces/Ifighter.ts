ca69b7b9c04b7be1969775920b3e0596eab1c4f3
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 19:09:59 -0300

    fix: change takeDamage param to attackStrength

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/interfaces/Ifighter.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/interfaces/Ifighter.ts
@@ -5,5 +5,5 @@ export interface IFighter extends IPlayer {
     strength: number;
     attackName: string;
     attack(enemy: IFighter): string;
-    takeDamage(enemy: IFighter): void;
+    takeDamage(attackStrength: number): void;
 }
