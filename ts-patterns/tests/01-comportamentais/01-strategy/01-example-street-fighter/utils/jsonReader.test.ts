a3da3f3c723ff2dc4a19b9cdcc0b0eeb55d17239
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 19:59:17 -0300

    test: fix path to read file

--- ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/utils/jsonReader.test.ts
+++ ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/utils/jsonReader.test.ts
@@ -1,8 +1,9 @@
 import { readJSONFile } from "../../../../../01-comportamentais/utils/jsonFileReader"
+import path from 'path'
 
 describe('Test JSON reader', ()=> {
     it('should read a JSON file', async()=> {
-        const filePath = './mockData.json'
+        const filePath = path.resolve(__dirname, 'mockData.json')
         const jsonData = await readJSONFile(filePath)
         expect(jsonData.version).toEqual(1.0)
     })
