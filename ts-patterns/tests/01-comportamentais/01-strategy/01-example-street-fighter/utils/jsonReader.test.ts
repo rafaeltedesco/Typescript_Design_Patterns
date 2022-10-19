c8225fc62fc113b417d4732383f4c508cb8dc0d0
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 19:55:14 -0300

    test: create test JSONReader

---
+++ ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/utils/jsonReader.test.ts
@@ -0,0 +1,8 @@
+
+describe('Test JSON reader', ()=> {
+    it('should read a JSON file', async()=> {
+        const filePath = './mockData.json'
+        const jsonData = await readJSONFile(filePath)
+        expect(jsonData.version).toEqual(1.0)
+    })
+})
