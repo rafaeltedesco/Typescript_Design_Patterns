5c2262c074a9463c9dad7b13672a1c3d82430cac
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 16:34:45 -0300

    test: import player and akuma

--- ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/unit/akuma.test.ts
+++ ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/unit/akuma.test.ts
@@ -1,4 +1,5 @@
-
+import { Player } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/interfaces/player"
+import { Akuma } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/players/akuma"
 
 describe('Test Akuma', ()=> {
     it('should create a player called Akuma', ()=> {
