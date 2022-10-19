7b5a73a93992dac177d208bb6c325e7d548938e7
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 20:53:00 -0300

    feat: fighters set path to load your own stories

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
@@ -1,9 +1,15 @@
 import { FightPlayer } from "../abstracts/fightPlayer";
+import path from 'path';
 
 export class Akuma extends FightPlayer {
     attackName = 'Gou-Hadoken'
-
+    
     constructor() {
         super('Akuma', 120, 60)
+        this.storyPath = path.resolve(
+            __dirname,
+            '../stories/akumaStory.json'
+        )
     }
+   
 }
--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/balrog.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/balrog.ts
@@ -1,10 +1,16 @@
-
 import { FightPlayer } from "../abstracts/fightPlayer";
+import path from 'path'
 
 export class Balrog extends FightPlayer {
     
     attackName = 'Dash Straight'
     constructor() {
         super('Balrog', 100, 40)
+        this.storyPath = path.resolve(
+            __dirname,
+            '../stories/akumaStory.json'
+        )
     }
+
+    
 }
