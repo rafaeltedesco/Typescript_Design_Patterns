d5ba1a372d228f3bded2c12bcb4488be08654c78
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-20 17:11:09 -0300

    test: should have balance equals to 300 when make two deposits of 150

--- ts-patterns/tests/01-comportamentais/01-strategy/02-example-payment-methods/unit/account.test.ts
+++ ts-patterns/tests/01-comportamentais/01-strategy/02-example-payment-methods/unit/account.test.ts
@@ -1,7 +1,17 @@
+import Account from "../../../../../01-comportamentais/01-strategy/02-example-payment-methods/domain/entities/account";
+
 describe("Test an Account", () => {
-  it("should create an account that can deposit values", () => {
-    const account = new Account();
-    account.deposit(100);
-    expect(account.balance).toEqual(100);
+  describe("Test Deposit", () => {
+    it("should create an account that can deposit values", () => {
+      const account = new Account();
+      account.deposit(100);
+      expect(account.balance).toEqual(100);
+    });
+    it("should have balance equals to 300 when make two deposits of 150", () => {
+      const account = new Account();
+      account.deposit(150);
+      account.deposit(150);
+      expect(account.balance).toEqual(300);
+    });
   });
 });
