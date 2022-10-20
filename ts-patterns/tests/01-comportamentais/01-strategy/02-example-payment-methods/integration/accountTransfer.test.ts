0db1acd7c9ce629e68482ae362e159865592a97c
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-20 17:24:51 -0300

    test: should make a transfer to another account

---
+++ ts-patterns/tests/01-comportamentais/01-strategy/02-example-payment-methods/integration/accountTransfer.test.ts
@@ -0,0 +1,13 @@
+import Account from "../../../../../01-comportamentais/01-strategy/02-example-payment-methods/domain/entities/account";
+
+describe("Test Account Transfer", () => {
+  describe("Transfer to another Account", () => {
+    it("should make a transfer to another account", () => {
+      const account1 = new Account(500);
+      const account2 = new Account(700);
+      account1.transfer(account2, 200);
+      expect(account1.balance).toEqual(300);
+      expect(account2.balance).toEqual(1000);
+    });
+  });
+});
