038a3304e08c6de86f5efb584ba07eced848e3eb
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-20 11:23:59 -0300

    feat: create attacks implementations for each Player

---
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/attacks/akumaAttacks.ts
@@ -0,0 +1,8 @@
+import IAttack from "../../interfaces/Iattack";
+
+class AkumaAttacks implements IAttack {
+  attackName = "Gou-Hadoken";
+  specialAttackName = "Wrath of the Raging Demon";
+}
+
+export default AkumaAttacks;
---
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/attacks/balrogAttacks.ts
@@ -0,0 +1,8 @@
+import IAttack from "../../interfaces/Iattack";
+
+class BalrogAttacks implements IAttack {
+  attackName = "Dash Straight";
+  specialAttackName = "Buffalo Headbutt";
+}
+
+export default BalrogAttacks;
