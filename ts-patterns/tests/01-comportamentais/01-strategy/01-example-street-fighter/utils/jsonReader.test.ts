3d125d80f18291cda8c5a98890312e797988fa6a
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-20 14:28:02 -0300

    refactor: change export

--- ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/utils/jsonReader.test.ts
+++ ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/utils/jsonReader.test.ts
@@ -1,10 +1,10 @@
 import path from "path";
-import readJSONFile from "../../../../../01-comportamentais/utils/jsonFileReader";
+import reader from "../../../../../01-comportamentais/utils/jsonFileReader";
 
 describe("Test JSON reader", () => {
   it("should read a JSON file", async () => {
     const filePath = path.resolve(__dirname, "mockData.json");
-    const jsonData = await readJSONFile(filePath);
+    const jsonData = await reader.readJSONFile(filePath);
     expect(jsonData.version).toEqual(1.0);
   });
 });
