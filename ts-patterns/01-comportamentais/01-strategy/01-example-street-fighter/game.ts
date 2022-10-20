a3155747f00fb721d62dc5c0c180fe738a663258
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-20 12:19:21 -0300

    feat: add optional narrator to the game

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/game.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/game.ts
@@ -1,13 +1,16 @@
 import IFighter from "./interfaces/Ifighter";
+import INarrator from "./interfaces/InarratorPlayer";
 
 class Game {
   private _player1: IFighter;
   private _player2: IFighter;
+  private _narrator?: INarrator;
   private _hasNarrator = false;
 
-  constructor(player1: IFighter, player2: IFighter) {
+  constructor(player1: IFighter, player2: IFighter, narrator?: INarrator) {
     this._player1 = player1;
     this._player2 = player2;
+    if (narrator) this.narrator = narrator;
   }
 
   get player1(): IFighter {
@@ -20,6 +23,16 @@ class Game {
 
   get hasNarrator(): boolean {
     return this._hasNarrator;
+  }
+
+  get narrator(): INarrator {
+    if (!this._narrator) throw new Error("This Game has no Narrator");
+    return this._narrator;
+  }
+
+  set narrator(narrator: INarrator) {
+    this._narrator = narrator;
+    this._hasNarrator = true;
   }
 }
 
