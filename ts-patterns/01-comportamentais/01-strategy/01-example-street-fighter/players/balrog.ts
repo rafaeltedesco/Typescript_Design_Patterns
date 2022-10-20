93144da52626196cfa3707d44214313a1f1b345d
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-20 10:19:24 -0300

    fix: balrog lint

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/balrog.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/balrog.ts
@@ -1,11 +1,13 @@
-import path from 'path';
-import { FightPlayer } from '../abstracts/fightPlayer';
+import path from "path";
+import FightPlayer from "../abstracts/fightPlayer";
 
-export class Balrog extends FightPlayer {
-  attackName = 'Dash Straight';
+class Balrog extends FightPlayer {
+  attackName = "Dash Straight";
 
   constructor() {
-    super('Balrog', 100, 40);
-    this.storyPath = path.resolve(__dirname, '../stories/balrogStory.json');
+    super("Balrog", 100, 40);
+    this.storyPath = path.resolve(__dirname, "../stories/balrogStory.json");
   }
 }
+
+export default Balrog;
