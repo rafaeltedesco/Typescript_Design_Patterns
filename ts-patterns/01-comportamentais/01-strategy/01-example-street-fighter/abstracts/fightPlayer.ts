4ac51830bbc43b7ffc907e9aa850678111021bf1
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-20 11:25:18 -0300

    refactor: FightPlayer to accept moves and implement Attack using this new Object

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/abstracts/fightPlayer.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/abstracts/fightPlayer.ts
@@ -1,21 +1,27 @@
+import IAttack from "../interfaces/Iattack";
 import IFighter from "../interfaces/Ifighter";
 import Player from "./player";
 
 abstract class FightPlayer extends Player implements IFighter {
   lifePoints: number;
   strength: number;
-  abstract attackName: string;
-  abstract specialAttackName: string;
+  moves: IAttack;
 
-  constructor(name: string, lifePoints: number, strength: number) {
+  constructor(
+    name: string,
+    lifePoints: number,
+    strength: number,
+    moves: IAttack
+  ) {
     super(name);
     this.lifePoints = lifePoints;
     this.strength = strength;
+    this.moves = moves;
   }
 
   attack(enemy: IFighter): string {
     enemy.takeDamage(this.strength);
-    return `${this.name} attacks ${enemy.name} with ${this.attackName}!`;
+    return `${this.name} attacks ${enemy.name} with ${this.moves.attackName}!`;
   }
 
   takeDamage(attackStrength: number): void {
