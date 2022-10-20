7236d525b71952a1dddbbb8cbf09c8bceb2b31ec
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 21:08:51 -0300

    test: fix types

--- ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/unit/player.test.ts
+++ ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/unit/player.test.ts
@@ -1,13 +1,13 @@
 import { IFighter } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/interfaces/Ifighter"
 import { Narrator } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/players/narrator"
 import { FightPlayer } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/abstracts/fightPlayer"
-import { Player } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/abstracts/player"
+import { NarratorPlayer } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/abstracts/narratorPlayer"
 
 describe('Test Narrator', ()=> {
     
     describe('Test a Non Fighter Player', ()=> {
-        const player: Player = new Narrator()
-        const nonFighter: IFighter = player as FightPlayer
+        const player: NarratorPlayer = new Narrator()
+        const nonFighter: IFighter = player as unknown as FightPlayer
         it('should not have an attack method in a instance of Player', ()=> {
         expect(nonFighter.attack).toBe(undefined)
         })
