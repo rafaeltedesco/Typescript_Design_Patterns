f778c61b9a2304cd93a58dbf4b794060ef486ea5
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-19 21:13:39 -0300

    test: should tell Balrog Story

--- ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/integration/narrator.test.ts
+++ ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/integration/narrator.test.ts
@@ -2,6 +2,7 @@ import { Akuma } from "../../../../../01-comportamentais/01-strategy/01-example-
 import { Narrator } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/players/narrator"
 import path from 'path'
 import { readJSONFile } from "../../../../../01-comportamentais/utils/jsonFileReader"
+import { Balrog } from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/players/balrog"
 
 describe('Test Narrator', ()=> {
     describe('Narrator tells Akuma Story', ()=> {
@@ -22,6 +23,26 @@ describe('Test Narrator', ()=> {
             const akumaStory = (await readJSONFile(akumaStoryPath)).story
             await narrator.askForStory(akuma)
             expect(narrator.tellStory()).toEqual(akumaStory)
+        })
+    })
+    describe('Narrator tells Balrog Story', ()=> {
+        const narrator = new Narrator()
+        const relativePath = path.resolve(
+            __dirname,
+             ...".. ".repeat(5).split(" ")
+             )
+        const balrogStoryPath = path.resolve(
+            relativePath,
+            '01-comportamentais/01-strategy/',
+            "01-example-street-fighter/stories/",
+            "balrogStory.json"
+            )
+       
+        it('should tell Balrog Story', async ()=> {
+            const balrog = new Balrog()
+            const balrogStory = (await readJSONFile(balrogStoryPath)).story
+            await narrator.askForStory(balrog)
+            expect(narrator.tellStory()).toEqual(balrogStory)
         })
     })
 })
