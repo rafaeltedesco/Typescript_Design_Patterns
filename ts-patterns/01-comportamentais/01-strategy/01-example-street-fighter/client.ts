750f35ce3839478f7a6aaee4b156f4316cb5082f
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2022-10-21 14:39:44 -0300

    Update comment from client.ts

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/client.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/client.ts
@@ -8,11 +8,11 @@ import Narrator from "./players/narrator";
   const balrog = new Balrog();
   const narrator = new Narrator();
   const context = new Game(akuma, balrog, narrator);
-
+  
+  // Player been used as Strategy
   const akumaStory = await context.tellStory(akuma);
   const balrogStory = await context.tellStory(balrog);
 
-  // Player been used as Strategy
   process.stdout.write("Telling Akuma Story...\n\n");
   process.stdout.write(akumaStory);
   process.stdout.write("\n\n");
