abb401329a1b4cf9176a0446969c4124228b6ce8
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-21 10:58:35 -0300

    feat: create street-fighter client

---
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/client.ts
@@ -0,0 +1,22 @@
+import Game from "./game";
+import Akuma from "./players/akuma";
+import Balrog from "./players/balrog";
+import Narrator from "./players/narrator";
+
+(async () => {
+  const akuma = new Akuma();
+  const balrog = new Balrog();
+  const narrator = new Narrator();
+  const context = new Game(akuma, balrog, narrator);
+
+  const akumaStory = await context.tellStory(akuma);
+  const balrogStory = await context.tellStory(balrog);
+
+  // Player been used as Strategy
+  process.stdout.write("Telling Akuma Story...\n\n");
+  process.stdout.write(akumaStory);
+  process.stdout.write("\n\n");
+  process.stdout.write("Telling Balrog Story...\n\n");
+  process.stdout.write(balrogStory);
+  process.stdout.write("\n\n");
+})();
