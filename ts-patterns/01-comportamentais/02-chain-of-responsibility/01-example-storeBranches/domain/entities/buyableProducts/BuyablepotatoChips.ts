63d091bd41ba4b32efca32e2caf58d0164d6edf7
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-22 19:29:47 -0300

    feat: create buyable products

---
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/buyableProducts/BuyablepotatoChips.ts
@@ -0,0 +1,9 @@
+import { Buyable } from "../../interfaces/Iproduct";
+
+class PotatoChips implements Buyable {
+  price = 5.5;
+  quantity = 100;
+  name = "Potato Chips";
+}
+
+export default PotatoChips;
