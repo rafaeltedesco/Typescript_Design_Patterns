681d52e497e43580ce87a2423d3c94301ad4c9be
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-22 19:47:34 -0300

    test: add async to methods

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/integration/megaStore.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/integration/megaStore.test.ts
@@ -5,32 +5,32 @@ import PotatoChips from "../../../../../01-comportamentais/02-chain-of-responsib
 import TShirt from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/products/tShirt";
 
 describe("Test Mega Store", () => {
-  it("Mega Store should search for PotatoChips in Branches and return GroceryStore", () => {
+  it("Mega Store should search for PotatoChips in Branches and return GroceryStore", async () => {
     const megaStore = new MegaStore();
     const groceryStore = new GroceryStore();
     const clothingStore = new ClothingStore();
     clothingStore.setNext(groceryStore);
     megaStore.storeChain = clothingStore;
-    const store = megaStore.searchProductInBranches(new PotatoChips());
+    const store = await megaStore.searchProductInBranches(new PotatoChips());
     expect(store).toBeInstanceOf(GroceryStore);
   });
-  it("Mega Store should search for T-Shirt in Branches and return Clothing Store", () => {
+  it("Mega Store should search for T-Shirt in Branches and return Clothing Store", async () => {
     const megaStore = new MegaStore();
     const groceryStore = new GroceryStore();
     const clothingStore = new ClothingStore();
     clothingStore.setNext(groceryStore);
     megaStore.storeChain = clothingStore;
-    const store = megaStore.searchProductInBranches(new TShirt());
+    const store = await megaStore.searchProductInBranches(new TShirt());
     expect(store).toBeInstanceOf(ClothingStore);
   });
-  it("should call hasProduct in groceryStore once", () => {
+  it("should call hasProduct in groceryStore once", async () => {
     const megaStore = new MegaStore();
     const groceryStore = new GroceryStore();
     const clothingStore = new ClothingStore();
     clothingStore.setNext(groceryStore);
     megaStore.storeChain = clothingStore;
     const grocerySpy = jest.spyOn(groceryStore, "hasProduct");
-    megaStore.searchProductInBranches(new PotatoChips());
+    await megaStore.searchProductInBranches(new PotatoChips());
     expect(grocerySpy).toBeCalledTimes(1);
   });
 });
