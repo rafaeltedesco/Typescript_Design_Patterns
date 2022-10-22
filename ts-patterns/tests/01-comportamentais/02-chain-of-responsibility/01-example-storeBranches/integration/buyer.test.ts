42ed84a7f54149f1197c17bf30f610941c18b54c
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-22 18:41:23 -0300

    test: import T-Shirt

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/integration/buyer.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/integration/buyer.test.ts
@@ -3,6 +3,7 @@ import ClothingStore from "../../../../../01-comportamentais/02-chain-of-respons
 import GroceryStore from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/groceryStore";
 import MegaStore from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/megaStore";
 import PotatoChips from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/products/potatoChips";
+import TShirt from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/products/tShirt";
 
 describe("Test Buyer when interacting with other classes", () => {
   describe("Test Buyer when askingForProduct to MegaStore", () => {
