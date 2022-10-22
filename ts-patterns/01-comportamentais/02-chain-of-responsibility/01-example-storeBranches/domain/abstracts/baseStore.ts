14604eb33b9add902243c4aa8a472829578ffa27
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-22 18:33:45 -0300

    feat: create BaseStore abstract class

---
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/abstracts/baseStore.ts
@@ -0,0 +1,17 @@
+import { Buyable, IProduct } from "../interfaces/Iproduct";
+import StoreBranchHandler from "../interfaces/storeBranchHandler";
+
+abstract class BaseStore implements StoreBranchHandler {
+  abstract name: string;
+  phoneNumber?: string | undefined;
+  products: Buyable[] = [];
+  next: StoreBranchHandler | null = null;
+
+  abstract hasProduct(product: IProduct): Promise<boolean>;
+  abstract sellProduct(product: Buyable): Promise<void>;
+  setNext(store: StoreBranchHandler): void {
+    this.next = store;
+  }
+}
+
+export default BaseStore;
