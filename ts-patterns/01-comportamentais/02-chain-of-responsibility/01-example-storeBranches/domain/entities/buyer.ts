595d55d04dbb920f8af82e92c9f4cee1604f5dde
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-22 17:49:44 -0300

    feat: create a user with an email

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/buyer.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/buyer.ts
@@ -1,12 +1,19 @@
 class Buyer {
   private _name: string;
+  private _email: string | undefined;
 
-  constructor(name: string) {
+  constructor(name: string, email?: string) {
     this._name = name;
+    this._email = email;
   }
 
   get name(): string {
     return this._name;
+  }
+
+  get email(): string {
+    if (!this._email) return "Email is not defined";
+    return this._email;
   }
 }
 
