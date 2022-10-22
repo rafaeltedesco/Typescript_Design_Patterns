5e6365c3903dd0a15c98f4dd551c8a8c3f6315ea
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-22 19:02:10 -0300

    feat: implement methods

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/megaStore.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/megaStore.ts
@@ -1,3 +1,18 @@
-class MegaStore {}
+import { IProduct } from "../interfaces/Iproduct";
+import StoreBranchHandler from "../interfaces/storeBranchHandler";
+import GroceryStore from "./groceryStore";
+
+class MegaStore {
+  private _storeChain: StoreBranchHandler | null = null;
+
+  set storeChain(chain: StoreBranchHandler) {
+    this._storeChain = chain;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  searchProductInBrances(product: IProduct): StoreBranchHandler {
+    return new GroceryStore();
+  }
+}
 
 export default MegaStore;
