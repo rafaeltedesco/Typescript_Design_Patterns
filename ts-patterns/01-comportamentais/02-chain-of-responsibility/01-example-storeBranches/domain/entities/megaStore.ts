13afdb8ab5a0a246f2b80b8927a24d3eedf5d4a3
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-22 19:16:53 -0300

    feat: implement searchProductInBranches method

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/megaStore.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/megaStore.ts
@@ -1,7 +1,5 @@
 import { IProduct } from "../interfaces/Iproduct";
 import StoreBranchHandler from "../interfaces/storeBranchHandler";
-import ClothingStore from "./clothingStore";
-import GroceryStore from "./groceryStore";
 
 class MegaStore {
   private _storeChain: StoreBranchHandler | null = null;
@@ -10,11 +8,11 @@ class MegaStore {
     this._storeChain = chain;
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  searchProductInBrances(product: IProduct): StoreBranchHandler {
-    return product.name === "Potato Chips"
-      ? new GroceryStore()
-      : new ClothingStore();
+  async searchProductInBrances(
+    product: IProduct
+  ): Promise<StoreBranchHandler | null> {
+    const store = this._storeChain?.hasProduct(product);
+    return store || null;
   }
 }
 
