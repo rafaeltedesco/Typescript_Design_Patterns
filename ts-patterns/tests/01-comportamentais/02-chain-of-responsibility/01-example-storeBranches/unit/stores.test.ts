cfa0509a69e19325a44fe2ba2af2db1858ed288f
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-23 18:12:19 -0300

    test: add stores test

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/unit/stores.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/unit/stores.test.ts
@@ -2,6 +2,7 @@ import ClothingStore from "../../../../../01-comportamentais/02-chain-of-respons
 import GroceryStore from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/groceryStore";
 import PotatoChips from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/products/potatoChips";
 import TShirt from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/products/tShirt";
+import StoreBranchHandler from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/interfaces/storeBranchHandler";
 
 describe("Test Stores", () => {
   it("should set name if name is passed on constructor", () => {
@@ -19,5 +20,30 @@ describe("Test Stores", () => {
 
     expect(clothStore.hasProduct(new PotatoChips())).toEqual(null);
     expect(groceryStore.hasProduct(new TShirt())).toEqual(null);
+  });
+  it("should call next.hasProduct in Clothing Store when has next and product not found", () => {
+    const clothStore = new ClothingStore();
+    clothStore.setNext(new GroceryStore());
+    const grocerySpy = jest.spyOn(
+      clothStore.next as StoreBranchHandler,
+      "hasProduct"
+    );
+    clothStore.hasProduct(new PotatoChips());
+    expect(grocerySpy).toBeCalledTimes(1);
+  });
+  it("should call next.hasProduct in Grocery Store when has next and product not found", () => {
+    const groceryStore = new GroceryStore();
+    groceryStore.setNext(new ClothingStore());
+    const clothingSpy = jest.spyOn(
+      groceryStore.next as StoreBranchHandler,
+      "hasProduct"
+    );
+    groceryStore.hasProduct(new TShirt());
+    expect(clothingSpy).toBeCalledTimes(1);
+  });
+  it("should set balance on stores", () => {
+    const store = new GroceryStore();
+    store.balance = 10;
+    expect(store.balance).toEqual(10);
   });
 });
