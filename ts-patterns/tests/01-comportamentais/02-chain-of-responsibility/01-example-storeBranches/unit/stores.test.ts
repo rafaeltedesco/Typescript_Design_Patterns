77862c21fc70ea8f4c79b8465e5bdbd8294b84ff
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-23 17:45:37 -0300

    test: should return null if doesnt has next

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/unit/stores.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/unit/stores.test.ts
@@ -1,5 +1,7 @@
 import ClothingStore from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/clothingStore";
 import GroceryStore from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/groceryStore";
+import PotatoChips from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/products/potatoChips";
+import TShirt from "../../../../../01-comportamentais/02-chain-of-responsibility/01-example-storeBranches/domain/entities/products/tShirt";
 
 describe("Test Stores", () => {
   it("should set name if name is passed on constructor", () => {
@@ -10,5 +12,12 @@ describe("Test Stores", () => {
 
     expect(clothStore.name).toEqual(clothStoreName);
     expect(groceryStore.name).toEqual(groceryStoreName);
+  });
+  it("should return null if doesnt has next", () => {
+    const clothStore = new ClothingStore();
+    const groceryStore = new GroceryStore();
+
+    expect(clothStore.hasProduct(new PotatoChips())).toEqual(null);
+    expect(groceryStore.hasProduct(new TShirt())).toEqual(null);
   });
 });
