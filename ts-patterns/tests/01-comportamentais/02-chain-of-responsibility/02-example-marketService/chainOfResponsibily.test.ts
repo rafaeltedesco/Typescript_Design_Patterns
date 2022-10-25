409f79ebc4da7aa144ca365aba0869c2876e6510
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 20:45:39 -0300

    test: new market has to receive a MarketService and call method startService

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/chainOfResponsibily.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/chainOfResponsibily.test.ts
@@ -1,16 +1,25 @@
+import { Hamburger } from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods";
+import Market from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/market";
 import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
 import NonFitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/nonFitPerson";
 import MarketService from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/marketService";
+import FoodOutput from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/types/foodOutput";
 
 describe("Test Chain Of Responsibility", () => {
   describe("Test when called by MarketService", () => {
+    const market = new Market(new MarketService());
+    market.addFoods(new Hamburger(9));
     it("should FitPerson not buy and delegate to nonFitPerson that buys an Hamburger", () => {
       const fitPerson = new FitPerson("John Doe", 10);
       const nonFitPerson = new NonFitPerson("Also John Doe", 15);
+
+      const expected: FoodOutput = { name: "Hamburger", isHealthy: false };
+
       fitPerson.setNext(nonFitPerson);
 
-      const marketService = new MarketService();
-      marketService.start(fitPerson);
+      market.startService(fitPerson);
+      expect(fitPerson.boughtedFoods).toEqual([]);
+      expect(nonFitPerson.boughtedFoods).toEqual([expected]);
     });
   });
 });
