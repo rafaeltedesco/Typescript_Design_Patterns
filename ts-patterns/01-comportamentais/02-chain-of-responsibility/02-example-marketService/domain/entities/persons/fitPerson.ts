a137310adafda9d4be360bb84f76a0f9a1b1bf43
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 15:54:34 -0300

    feat: create buy method and boughtedProducts getter

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson.ts
@@ -1,7 +1,18 @@
+import Broccoli from "../foods/healthlyFoods/broccoli";
+
 class FitPerson {
   private _name: string;
+  private _boughtedProducts: Broccoli[] = [];
   constructor(name: string) {
     this._name = name;
+  }
+
+  buy(food: Broccoli) {
+    this._boughtedProducts.push(food);
+  }
+
+  get boughtedProducts(): Broccoli[] {
+    return [...this._boughtedProducts];
   }
 }
 
