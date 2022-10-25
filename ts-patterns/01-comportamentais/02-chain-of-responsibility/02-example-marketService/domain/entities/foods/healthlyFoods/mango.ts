a8f963cb1ff28eba30f7570e33be7799a4b0bdc9
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 16:25:41 -0300

    feat: make classes implements IFood

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods/mango.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods/mango.ts
@@ -1,2 +1,9 @@
-class Mango {}
+import IFood from "../../../interfaces/ifood";
+
+class Mango implements IFood {
+  name = "Mango";
+  price = 4.8;
+  quantity = 7;
+  isHealthy = true;
+}
 export default Mango;
