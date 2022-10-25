13ef087bd9c57eed433dbc5d799b6cfbab483b38
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 16:02:14 -0300

    test: add healthly foods import

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
@@ -1,5 +1,8 @@
 import Broccoli from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods/broccoli";
 import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
+import Mango from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods/mango";
+import OrangeJuice from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods/orangeJuice";
+import Apple from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods/apple";
 
 describe("Test Fit Person", () => {
   it("should buy healthly products", () => {
