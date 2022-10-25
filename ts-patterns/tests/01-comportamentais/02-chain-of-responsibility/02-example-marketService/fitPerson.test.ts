dde52bc2f29baa19a7192f861a7693fd7219367b
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 16:30:19 -0300

    test: FitPerson should not buy unHealthy food

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
@@ -8,8 +8,8 @@ import {
 import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
 
 describe("Test Fit Person", () => {
+  const fitPerson = new FitPerson("John Doe");
   it("should buy healthly products", () => {
-    const fitPerson = new FitPerson("John Doe");
     const broccoli = new Broccoli();
     const mango = new Mango();
     const orangeJuice = new OrangeJuice();
@@ -19,5 +19,10 @@ describe("Test Fit Person", () => {
     expect(fitPerson.boughtedFoods).toEqual(healthlyFoods);
     fitPerson.boughtedFoods.forEach(food => food.isHealthy);
     expect(fitPerson.name).toEqual("John Doe");
+  });
+  it("should not buy unHealthy food", () => {
+    const hamburger = new Hamburger();
+    fitPerson.buy(hamburger);
+    expect(fitPerson.boughtedFoods).not.toContain(hamburger);
   });
 });
