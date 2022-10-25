04328cd65040d74510cbc8aba5e386ba3c89ff71
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 18:08:36 -0300

    test: NonFitPerson cannot buy Hamburger of a list of foods when don't have enough money

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/basePerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/basePerson.test.ts
@@ -4,7 +4,14 @@ import {
   Mango,
   OrangeJuice,
 } from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods";
+import {
+  FrenchFries,
+  Hamburger,
+  Pizza,
+  Snacks,
+} from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods";
 import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
+import NonFitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/nonFitPerson";
 
 describe("Test Base Person behavior in classes that extends it", () => {
   describe("FitPerson cannot buy Broccoli of a list of foods when don't have enough money", () => {
@@ -15,10 +22,24 @@ describe("Test Base Person behavior in classes that extends it", () => {
       new Apple(2.7),
       broccoli,
     ];
-    it("should not buy if don't have enough money", () => {
+    it("should not buy Broccoli if don't have enough money", () => {
       const fitPerson = new FitPerson("John Doe", 18);
       healthyFoods.forEach(food => fitPerson.buy(food));
       expect(fitPerson.boughtedFoods).not.toContain(broccoli);
+    });
+  });
+  describe("NonFitPerson cannot buy Hamburger of a list of foods when don't have enough money", () => {
+    const hamburger = new Hamburger(42);
+    const unHealthyFoods = [
+      new FrenchFries(5),
+      new Snacks(2),
+      new Pizza(22),
+      hamburger,
+    ];
+    it("should not buy hamburger if don't have enough money", () => {
+      const nonFitPerson = new NonFitPerson("John Doe", 35);
+      unHealthyFoods.forEach(food => nonFitPerson.buy(food));
+      expect(nonFitPerson.boughtedFoods).not.toContain(hamburger);
     });
   });
 });
