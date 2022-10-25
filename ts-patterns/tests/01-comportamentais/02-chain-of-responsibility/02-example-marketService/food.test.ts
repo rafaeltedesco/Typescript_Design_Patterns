c169445fdf8e168f6712c7a7c44cf138c02b8070
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 19:34:46 -0300

    test: should not buy any food if quantity is unavailable

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/food.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/food.test.ts
@@ -1,13 +1,43 @@
-import { Apple } from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods";
+import {
+  Apple,
+  Broccoli,
+  Mango,
+  OrangeJuice,
+} from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods";
+import {
+  FrenchFries,
+  Hamburger,
+  Pizza,
+  Snacks,
+} from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods";
 
 import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
+import NonFitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/nonFitPerson";
 
 describe("Test Food", () => {
-  it("should not buy an Apple if quantity is unavailable", () => {
-    const apple = new Apple(3, 1);
-    const fitPerson = new FitPerson("John Doe", 10);
-    fitPerson.buy(apple);
-    fitPerson.buy(apple);
-    expect(fitPerson.boughtedFoods).toEqual([apple]);
+  const healthlyFoods = [
+    new Apple(3),
+    new OrangeJuice(4),
+    new Mango(2),
+    new Broccoli(4),
+  ];
+  const unHealthlyFoods = [
+    new Hamburger(10),
+    new Snacks(2),
+    new FrenchFries(5),
+    new Pizza(10),
+  ];
+  it("should not buy any food if quantity is unavailable", () => {
+    const fitPerson = new FitPerson("John Doe", 999);
+    const nonFitPerson = new NonFitPerson("Also John Doe", 999);
+
+    healthlyFoods.forEach(food => fitPerson.buy(food));
+    healthlyFoods.forEach(food => fitPerson.buy(food));
+
+    unHealthlyFoods.forEach(food => nonFitPerson.buy(food));
+    unHealthlyFoods.forEach(food => nonFitPerson.buy(food));
+
+    expect(fitPerson.boughtedFoods).toEqual(healthlyFoods);
+    expect(nonFitPerson.boughtedFoods).toEqual(unHealthlyFoods);
   });
 });
