32664fd4c2e9cd03e060c6fe56b1a814947bde3c
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 20:25:49 -0300

    test: should add foods

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/market.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/market.test.ts
@@ -1,14 +1,46 @@
+import {
+  Apple,
+  Broccoli,
+  Mango,
+  OrangeJuice,
+} from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods";
+import {
+  FrenchFries,
+  Hamburger,
+  Pizza,
+  Snacks,
+} from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods";
 import Market from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/market";
 
 describe("Test Market", () => {
+  const market = new Market();
+  const healthlyFoods = [
+    new Apple(3),
+    new OrangeJuice(4),
+    new Mango(2),
+    new Broccoli(4),
+  ];
+  const unHealthlyFoods = [
+    new Hamburger(10),
+    new Snacks(2),
+    new FrenchFries(5),
+    new Pizza(10),
+  ];
+
   describe("Test Market creation", () => {
     it("should have a property balance with initial value equal to 0", () => {
-      const market = new Market();
       expect(market.balance).toEqual(0);
     });
     it("should start with no foods", () => {
-      const market = new Market();
       expect(market.foods).toEqual([]);
+    });
+  });
+  describe("Test if can add foods", () => {
+    it("should add foods", () => {
+      market.addFoods(...healthlyFoods);
+      expect(market.foods).toEqual(healthlyFoods);
+      market.addFoods(...unHealthlyFoods);
+      expect(market.foods).toEqual([...healthlyFoods, ...unHealthlyFoods]);
     });
   });
 });
