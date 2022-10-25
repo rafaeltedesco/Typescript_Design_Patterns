c0a9e08b4e05920a1868b13178d00dc7bdbd54b3
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 20:54:23 -0300

    feat: implement method start

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/marketService.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/marketService.ts
@@ -1,9 +1,11 @@
+import IFood from "../interfaces/ifood";
 import IPerson from "../interfaces/iperson";
 
 class MarketService {
-  ok = true;
-  start(person: IPerson): boolean {
-    return this.ok;
+  private _availableFoods: IFood[] = [];
+
+  start(person: IPerson): void {
+    this._availableFoods.forEach(food => person.buy(food));
   }
 }
 
