bc2a563808eb4e67d5a17ff0bef77ac12f90ad38
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 20:17:57 -0300

    test: import class market

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/market.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/market.test.ts
@@ -1,3 +1,5 @@
+import Market from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/market";
+
 describe("Test Market", () => {
   describe("Test Market creation", () => {
     it("should have a property balance with initial value equal to 0", () => {
