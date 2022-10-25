de60849ca536912a98aa3f6169bb633d4a03dc61
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 17:08:27 -0300

    test: refactor tests

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
@@ -4,26 +4,36 @@ import {
   OrangeJuice,
   Apple,
 } from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods";
+import FrenchFries from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/frenchFries";
 import Hamburger from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/hamburger";
+import Pizza from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/pizza";
+import Snacks from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/snacks";
 
 import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
 
 describe("Test Fit Person", () => {
   const fitPerson = new FitPerson("John Doe");
+  const healthyFoods = [
+    new Broccoli(),
+    new Mango(),
+    new OrangeJuice(),
+    new Apple(),
+  ];
+  const unhealthlyFoods = [
+    new Hamburger(),
+    new Pizza(),
+    new FrenchFries(),
+    new Snacks(),
+  ];
+
   it("should buy healthly products", () => {
-    const broccoli = new Broccoli();
-    const mango = new Mango();
-    const orangeJuice = new OrangeJuice();
-    const apple = new Apple();
-    const healthlyFoods = [broccoli, mango, orangeJuice, apple];
-    healthlyFoods.forEach(food => fitPerson.buy(food));
-    expect(fitPerson.boughtedFoods).toEqual(healthlyFoods);
+    healthyFoods.forEach(food => fitPerson.buy(food));
+    expect(fitPerson.boughtedFoods).toEqual(healthyFoods);
     fitPerson.boughtedFoods.forEach(food => food.isHealthy);
     expect(fitPerson.name).toEqual("John Doe");
   });
   it("should not buy unHealthy food", () => {
-    const hamburger = new Hamburger();
-    fitPerson.buy(hamburger);
-    expect(fitPerson.boughtedFoods).not.toContain(hamburger);
+    unhealthlyFoods.forEach(food => fitPerson.buy(food));
+    expect(fitPerson.boughtedFoods).toEqual(healthyFoods);
   });
 });
--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/nonFitPerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/nonFitPerson.test.ts
@@ -1,4 +1,7 @@
 import Hamburger from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/hamburger";
+import Pizza from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/pizza";
+import FrenchFries from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/frenchFries";
+import Snacks from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/snacks";
 import NonFitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/nonFitPerson";
 import IFood from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/interfaces/ifood";
 import {
@@ -10,21 +13,26 @@ import {
 
 describe("Test Non Fit Person", () => {
   const nonFitPerson = new NonFitPerson("John Doe");
-  const hamburger = new Hamburger();
+  const unhealthlyFoods = [
+    new Hamburger(),
+    new Pizza(),
+    new FrenchFries(),
+    new Snacks(),
+  ];
+  const healthyFoods: IFood[] = [
+    new Broccoli(),
+    new Mango(),
+    new Apple(),
+    new OrangeJuice(),
+  ];
+
   it("should buy unHealthly products", () => {
-    nonFitPerson.buy(hamburger);
-    expect(nonFitPerson.boughtedFoods).toContain(hamburger);
+    unhealthlyFoods.forEach(food => nonFitPerson.buy(food));
+    expect(nonFitPerson.boughtedFoods).toEqual(unhealthlyFoods);
     expect(nonFitPerson.name).toEqual("John Doe");
   });
   it("should not buy healthy foods", () => {
-    const healthyFoods: IFood[] = [
-      new Broccoli(),
-      new Mango(),
-      new Apple(),
-      new OrangeJuice(),
-    ];
-
     healthyFoods.forEach(food => nonFitPerson.buy(food));
-    expect(nonFitPerson.boughtedFoods).toEqual([hamburger]);
+    expect(nonFitPerson.boughtedFoods).toEqual(unhealthlyFoods);
   });
 });
