3f5d4a1342f654734065ee9de75657dbc076294a
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 20:20:37 -0300

    feat: create foods property

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/market.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/market.ts
@@ -1,8 +1,15 @@
+import IFood from "../interfaces/ifood";
+
 class Market {
   private _balance = 0;
+  private _foods: IFood[] = [];
 
   get balance(): number {
     return this._balance;
+  }
+
+  get foods(): IFood[] {
+    return [...this._foods];
   }
 }
 
