5f98f6bcd5e5fa8efc71d39b7effa58cb9f55f33
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 15:51:57 -0300

    test: import Broccoli and fix FitPerson import

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
@@ -1,4 +1,5 @@
-import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/fitPerson";
+import Broccoli from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods/broccoli";
+import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
 
 describe("Test Fit Person", () => {
   it("should buy healthly products", () => {
