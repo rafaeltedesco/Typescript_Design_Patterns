e5a116a8e7f6683e7cddc460a1e9c828c8f1a0cc
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 18:46:33 -0300

    refactor: move non variant methods and properties to super class

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/abstracts/basePerson.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/abstracts/basePerson.ts
@@ -1,3 +1,34 @@
-abstract class BasePerson {}
+import IFood from "../../../interfaces/ifood";
+
+abstract class BasePerson {
+  protected _money = 0;
+  protected _name: string;
+  protected _boughtedFoods: IFood[] = [];
+  constructor(name: string, money?: number) {
+    this._name = name;
+    if (money) this._money = money;
+  }
+
+  get money(): number {
+    return this._money;
+  }
+
+  buy(food: IFood) {
+    this._money -= food.price;
+    this._boughtedFoods.push(food);
+  }
+
+  protected hasEnoughMoney(food: IFood): boolean {
+    return this._money - food.price >= 0;
+  }
+
+  get boughtedFoods(): IFood[] {
+    return [...this._boughtedFoods];
+  }
+
+  get name(): string {
+    return this._name;
+  }
+}
 
 export default BasePerson;
--- ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/nonFitPerson.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/nonFitPerson.ts
@@ -2,37 +2,10 @@ import IFood from "../../interfaces/ifood";
 import BasePerson from "./abstracts/basePerson";
 
 class NonFitPerson extends BasePerson {
-  private _money = 0;
-  private _name: string;
-  private _boughtedFoods: IFood[] = [];
-
-  constructor(name: string, money?: number) {
-    super();
-    this._name = name;
-    if (money) this._money = money;
-  }
-
-  get money(): number {
-    return this._money;
-  }
-
   buy(food: IFood) {
     if (!food.isHealthy && this.hasEnoughMoney(food)) {
-      this._money -= food.price;
-      this._boughtedFoods.push(food);
+      super.buy(food);
     }
-  }
-
-  private hasEnoughMoney(food: IFood): boolean {
-    return this._money - food.price >= 0;
-  }
-
-  get boughtedFoods(): IFood[] {
-    return [...this._boughtedFoods];
-  }
-
-  get name(): string {
-    return this._name;
   }
 }
 
