92434e305d03176af7910bb7d571431260109389
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 20:40:21 -0300

    test: create test suite - should FitPerson not buy and delegate to nonFitPerson that buys an Hamburger

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/chainOfResponsibily.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/chainOfResponsibily.test.ts
@@ -1,13 +1,16 @@
 import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
 import NonFitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/nonFitPerson";
+import MarketService from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/marketService";
 
 describe("Test Chain Of Responsibility", () => {
   describe("Test when called by MarketService", () => {
-    const fitPerson = new FitPerson("John Doe", 10);
-    const nonFitPerson = new NonFitPerson("Also John Doe", 15);
-    fitPerson.setNext(nonFitPerson);
+    it("should FitPerson not buy and delegate to nonFitPerson that buys an Hamburger", () => {
+      const fitPerson = new FitPerson("John Doe", 10);
+      const nonFitPerson = new NonFitPerson("Also John Doe", 15);
+      fitPerson.setNext(nonFitPerson);
 
-    const marketService = new MarketService();
-    marketService.start(fitPerson);
+      const marketService = new MarketService();
+      marketService.start(fitPerson);
+    });
   });
 });
