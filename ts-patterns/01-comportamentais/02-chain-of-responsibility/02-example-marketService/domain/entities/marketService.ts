be65311c13c091cccabc836be5716fb1cf234d74
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 20:46:39 -0300

    chore: move marketService to entities folder

---
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/marketService.ts
@@ -0,0 +1,10 @@
+import IPerson from "../interfaces/iperson";
+
+class MarketService {
+  ok = true;
+  start(person: IPerson): boolean {
+    return this.ok;
+  }
+}
+
+export default MarketService;
