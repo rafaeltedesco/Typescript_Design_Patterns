6f40752aecd95bd785d766a1465974cd17525402
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 16:45:54 -0300

    test: change toEqual to toContain

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/nonFitPerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/nonFitPerson.test.ts
@@ -1,11 +1,12 @@
 import Hamburger from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/unHealthyFoods/hamburger";
+import NonFitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/nonFitPerson";
 
 describe("Test Non Fit Person", () => {
   const nonFitPerson = new NonFitPerson("John Doe");
   it("should buy unHealthly products", () => {
     const hamburger = new Hamburger();
     nonFitPerson.buy(hamburger);
-    expect(nonFitPerson.boughtedFoods).toEqual(hamburger);
+    expect(nonFitPerson.boughtedFoods).toContain(hamburger);
     expect(nonFitPerson.name).toEqual("John Doe");
   });
 });
