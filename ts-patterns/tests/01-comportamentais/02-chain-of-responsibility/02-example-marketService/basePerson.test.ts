fd9db5c639cc05fb6d1383c1578564f4cc8a014a
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 17:48:13 -0300

    test: FitPerson cannot buy Broccoli of a list of foods when don't have enough money

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/basePerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/basePerson.test.ts
@@ -1,13 +1,24 @@
-import { OrangeJuice } from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods";
+import {
+  Apple,
+  Broccoli,
+  Mango,
+  OrangeJuice,
+} from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/foods/healthlyFoods";
 import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/persons/fitPerson";
 
 describe("Test Base Person behavior in classes that extends it", () => {
-  describe("FitPerson cannot buy if don't have enough money", () => {
+  describe("FitPerson cannot buy Broccoli of a list of foods when don't have enough money", () => {
+    const broccoli = new Broccoli(3.2);
+    const healthyFoods = [
+      new OrangeJuice(10),
+      new Mango(5),
+      new Apple(2.7),
+      broccoli,
+    ];
     it("should not buy if don't have enough money", () => {
-      const fitPerson = new FitPerson("John Doe", 10);
-      const orangeJuice = new OrangeJuice(12);
-      fitPerson.buy(orangeJuice);
-      expect(fitPerson.boughtedFoods).not.toContain(orangeJuice);
+      const fitPerson = new FitPerson("John Doe", 18);
+      healthyFoods.forEach(food => fitPerson.buy(food));
+      expect(fitPerson.boughtedFoods).not.toContain(broccoli);
     });
   });
 });
