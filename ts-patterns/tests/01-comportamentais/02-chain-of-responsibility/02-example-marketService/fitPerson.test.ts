9ecab105d734537fee577ddc6c4c4272df512a55
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 15:49:39 -0300

    test: import a fitperson

--- ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
+++ ts-patterns/tests/01-comportamentais/02-chain-of-responsibility/02-example-marketService/fitPerson.test.ts
@@ -1,3 +1,5 @@
+import FitPerson from "../../../../01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/fitPerson";
+
 describe("Test Fit Person", () => {
   it("should buy healthly products", () => {
     const fitPerson = new FitPerson("John Doe");
