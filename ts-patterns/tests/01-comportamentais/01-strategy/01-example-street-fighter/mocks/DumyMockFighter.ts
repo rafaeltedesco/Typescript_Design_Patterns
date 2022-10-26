1f86a8fc5c0a4757eb79bde57f9d24b05e3faee0
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-26 20:00:52 -0300

    fix: dummy fakemock

--- ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/mocks/DumyMockFighter.ts
+++ ts-patterns/tests/01-comportamentais/01-strategy/01-example-street-fighter/mocks/DumyMockFighter.ts
@@ -1,5 +1,10 @@
 import FightPlayer from "../../../../../01-comportamentais/01-strategy/01-example-street-fighter/abstracts/fightPlayer";
 
-class FakeMockPlayer extends FightPlayer {}
+class FakeMockPlayer extends FightPlayer {
+  // eslint-disable-next-line class-methods-use-this
+  async loadStory(): Promise<string> {
+    return "";
+  }
+}
 
 export default FakeMockPlayer;
