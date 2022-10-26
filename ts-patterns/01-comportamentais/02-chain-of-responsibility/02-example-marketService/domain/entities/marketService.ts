b103b4cb11aaa5c72bdd2eb49f6dddac7ffc22d6
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-26 10:02:12 -0300

    feat: create a balance accumulator for the service

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/marketService.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/marketService.ts
@@ -3,13 +3,19 @@ import IPerson from "../interfaces/iperson";
 
 class MarketService {
   private _availableFoods: IFood[] = [];
+  private balanceAccumulator = 0;
 
   defineFoodsToService(...foods: IFood[]) {
     this._availableFoods = [...foods];
   }
 
-  start(person: IPerson): void {
-    this._availableFoods.forEach(food => person.buy(food));
+  start(person: IPerson): number {
+    this._availableFoods.forEach(food => {
+      if (person.buy(food)) {
+        this.balanceAccumulator += food.price;
+      }
+    });
+    return this.balanceAccumulator;
   }
 }
 
