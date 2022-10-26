42116955199ee773ada9a2fd81b376a595d786ef
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-25 21:17:01 -0300

    feat: create defineFoodsToService method

--- ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/marketService.ts
+++ ts-patterns/01-comportamentais/02-chain-of-responsibility/02-example-marketService/domain/entities/marketService.ts
@@ -4,6 +4,10 @@ import IPerson from "../interfaces/iperson";
 class MarketService {
   private _availableFoods: IFood[] = [];
 
+  defineFoodsToService(...foods: IFood[]) {
+    this._availableFoods = [...foods];
+  }
+
   start(person: IPerson): void {
     this._availableFoods.forEach(food => person.buy(food));
   }
