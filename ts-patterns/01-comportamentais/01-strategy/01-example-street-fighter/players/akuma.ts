8c227c1b9e5933f0aa54887be2cc0da5ed04023c
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-10-26 19:11:00 -0300

    feat: add different implementations for loadStory for each class to explain better one context to use Strategy

--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/akuma.ts
@@ -1,11 +1,17 @@
 import path from "path";
 import FightPlayer from "../abstracts/fightPlayer";
 import AkumaAttacks from "./attacks/akumaAttacks";
+import reader from "../../../../utils/jsonFileReader";
 
 class Akuma extends FightPlayer {
   constructor() {
     super("Akuma", 120, 60, new AkumaAttacks());
     this.storyPath = path.resolve(__dirname, "../stories/akumaStory.json");
+  }
+
+  async loadStory(): Promise<string> {
+    this.story = (await reader.readJSONFile(this.storyPath)).story;
+    return this.story;
   }
 }
 
--- ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/balrog.ts
+++ ts-patterns/01-comportamentais/01-strategy/01-example-street-fighter/players/balrog.ts
@@ -1,11 +1,25 @@
 import path from "path";
 import FightPlayer from "../abstracts/fightPlayer";
 import BalrogAttacks from "./attacks/balrogAttacks";
+import reader from "../../../../utils/jsonFileReader";
 
 class Balrog extends FightPlayer {
   constructor() {
     super("Balrog", 100, 40, new BalrogAttacks());
     this.storyPath = path.resolve(__dirname, "../stories/balrogStory.json");
+  }
+
+  private static async formatOutput(
+    storyDataToFormat: Record<string, string>
+  ): Promise<{ story: string; font: string }> {
+    const { CharacterStory: story, site: font } = storyDataToFormat;
+    return { story, font };
+  }
+
+  async loadStory(): Promise<string> {
+    const [tempStory] = await reader.readJSONFile(this.storyPath);
+    this.story = (await Balrog.formatOutput(tempStory)).story;
+    return this.story;
   }
 }
 
